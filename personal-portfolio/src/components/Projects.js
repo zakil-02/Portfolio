2580e1fdc4485ce1b8a5e2aa2272a0dca83140f5
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-12-26 05:18:32 +0100

    Adding programming languqges skills

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -1,98 +1,89 @@
+import React, { useState } from "react";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import maze from "../assets/img/maze1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
+import Carousel from "react-bootstrap/Carousel";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Business Startup",
+      title: "Maze Game",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrls: [maze, projImg2],
     },
     {
-      title: "Business Startup",
+      title: "Maze Game",
       description: "Design & Development",
-      imgUrl: projImg2,
+      imgUrls: [maze, projImg2],
     },
     {
-      title: "Business Startup",
+      title: "Maze Game",
       description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrls: [maze, projImg2],
     },
     {
-      title: "Business Startup",
+      title: "Maze Game",
       description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrls: [maze, projImg2],
     },
   ];
+
+  const [activeTab, setActiveTab] = useState("first");
 
   return (
     <section className="project" id="project">
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
+          <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+            <h2>Projects</h2>
+            <p>Here are some of the projects I contributed to.</p>
+            <Tab.Container id="projects-tabs" activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                {projects.map((project, index) => (
+                  <Nav.Item key={index}>
+                    <Nav.Link eventKey={`tab-${index + 1}`}>{project.title}</Nav.Link>
+                  </Nav.Item>
+                ))}
+              </Nav>
+              <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                {projects.map((project, index) => (
+                  <Tab.Pane key={index} eventKey={`tab-${index + 1}`}>
+                    <Row>
+                      <Col md={6}>
+                        <Carousel>
+                          {project.imgUrls.map((imgUrl, imgIndex) => (
+                            <Carousel.Item key={imgIndex}>
+                              <img src={imgUrl} alt={`Project ${index + 1} image ${imgIndex + 1}`} className="img-fluid" />
+                            </Carousel.Item>
+                          ))}
+                        </Carousel>
+                      </Col>
+                      <Col md={6}>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        {/* Additional project details can be added here */}
+                      </Col>
+                    </Row>
+                  </Tab.Pane>
+                ))}
+              </Tab.Content>
+            </Tab.Container>
+            </div>}
+          </TrackVisibility>
+        </Col>
+      </Row>
+    </Container>
+      <img className="background-image-right" src={colorSharp2} alt="Background" />
     </section>
-  )
-}
+  );
+};
+
+export default Projects;
--- personal-portfolio/src/components/prgL.js
+++ personal-portfolio/src/components/prgL.js
@@ -1,24 +1,47 @@
 
 import React from 'react';
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
+//import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+//import { ProjectCard } from "./ProjectCard";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+//import TrackVisibility from 'react-on-screen';
 
-import './ProgrammingLanguages.css'; // Make sure this points to your CSS file
-import reactImage from './assets/img/react.png'; // Replace with your image paths
-import pythonImage from './assets/img/python.png';
-// Import other language images here
+//import './ProgrammingLanguages.css'; // Make sure this points to your CSS file
+import reactImage from '../assets/img/react.png';
+import jImage from '../assets/img/java.png';
+import jsImage from '../assets/img/js.png';
+import cImage from '../assets/img/c.png';
+import cplusImage from '../assets/img/c++.png';
+import cssImage from '../assets/img/css.png';
+import htmlImage from '../assets/img/html.png';
+import soliImage from '../assets/img/solidity.png';
+import rImage from '../assets/img/R.svg.png';
+import pyImage from '../assets/img/py.png';
+import matlabImage from '../assets/img/Matlab.png'
+
 
 const languages = [
+  { name: 'Python', image: pyImage },
+  { name: 'R language', image: rImage },
+  { name: 'matlab', image: matlabImage },
+  { name: 'Java', image: jImage },
+  { name: 'C', image: cImage },
+  { name: 'C++', image: cplusImage },
+  { name: 'html', image: htmlImage },
+  { name: 'css', image: cssImage },
+  { name: 'JavaScript', image: jsImage },
   { name: 'React', image: reactImage },
-  { name: 'Python', image: pythonImage },
-  // Add other languages here
+  { name: 'Solidity', image: soliImage },
+  
+  
 ];
 
 const ProgrammingLanguages = () => {
   return (
     <div className="language-container">
+      <div className="title-container"> {/* Added container for title */}
+        <h2 className="title">Programming Languages</h2>
+      </div>
+      
       {languages.map((language, index) => (
         <div className="language-item" key={index}>
           <img
@@ -34,5 +57,4 @@ const ProgrammingLanguages = () => {
 };
 
 export default ProgrammingLanguages;
-
 
